3c5f4255068d4db84f8f2c1cf4a12462a4e5fb12
Yannic Remmet <yremmet@evoila.de>
2017-08-01 09:54:01 +0200

    chrore(move) moves java sources to correct directory

---
+++ osb-service/src/main/java/de/evoila/cf/broker/service/custom/MySQLExistingServiceFactory.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package de.evoila.cf.broker.service.custom;
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import de.evoila.cf.broker.exception.PlatformException;
+import de.evoila.cf.broker.service.mysql.MySQLCustomImplementation;
+import de.evoila.cf.broker.service.mysql.jdbc.MySQLDbService;
+import de.evoila.cf.cpi.existing.CustomExistingService;
+import de.evoila.cf.cpi.existing.CustomExistingServiceConnection;
+import de.evoila.cf.cpi.existing.ExistingServiceFactory;
+
+/**
+ * @author Christian Brinker, evoila.
+ *
+ */
+@Service
+@ConditionalOnProperty(prefix="existing.endpoint", name={"hosts","port","username","password","database"},havingValue="")
+public class MySQLExistingServiceFactory extends ExistingServiceFactory {
+	
+	@Autowired
+	private MySQLCustomImplementation mySQLCustomImplementation;
+	
+	protected CustomExistingService getCustomExistingService() {
+		return mySQLCustomImplementation;
+	}
+
+	public void createDatabase(MySQLDbService connection, String database) throws PlatformException {
+		try {
+			connection.executeUpdate("CREATE DATABASE `" + database + "`");
+			//connection.executeUpdate("REVOKE all on database " + database + " from public");
+		} catch (SQLException e) {
+			log.error(e.toString());
+			throw new PlatformException("Could not add to database");
+		}
+	}
+
+	public void deleteDatabase(MySQLDbService connection, String database) throws PlatformException {
+		try {
+			//connection.executeUpdate("REVOKE all on database \"" + database + "\" from public");
+			connection.executeUpdate("DROP DATABASE `" + database + "`");
+		} catch (SQLException e) {
+			log.error(e.toString());
+			throw new PlatformException("Could not remove from database");
+		}
+	}
+
+	@Override
+	protected void deleteInstance(CustomExistingServiceConnection connection, String instanceId) throws PlatformException {
+		if(connection instanceof MySQLDbService)
+			deleteDatabase((MySQLDbService) connection, instanceId);
+	}
+
+	@Override
+	protected void createInstance(CustomExistingServiceConnection connection, String instanceId) throws PlatformException {
+		if(connection instanceof MySQLDbService)
+			createDatabase((MySQLDbService) connection, instanceId);
+	}
+}
