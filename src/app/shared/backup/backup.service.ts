54c2338bbc1d3f3903523e07ae21998c29568b9f
Yannic Remmet <yremmet@evoila.de>
2017-08-03 16:40:50 +0200

    Squashed 'osb-dashboard/' changes from 551b400..33a6a38
    33a6a38 Merge branch 'master' of github.com:evoila/cf-service-broker-dashboard
    85929bc feat(var-injection): variables injection for delivery with Thymeleaf
    0110cd2 feat(backup): added serveral detail components and the possibility to trigger a restore process
    c1d0da7 feat(details): added basic details view
    587fb59 fix(backup-components): added list of Jobs and Plans. Added creation of Job
    
    git-subtree-dir: osb-dashboard
    git-subtree-split: 33a6a3854d6c626b723ec244741ad682f9db67c3

--- src/app/shared/backup/backup.service.ts
+++ src/app/shared/backup/backup.service.ts
@@ -8,32 +8,61 @@ import { Job } from './domain/job';
 
 @Injectable()
 export class BackupService {
-
+  BACKUP_BASEURL = '/v2/backup';
   constructor(protected readonly httpService: CoreHttpService) {}
 
-  public loadBackupPlans(): Observable<{} | BackupPlan> {
+  public loadBackupPlans(instanceId: string): Observable<{} | BackupPlan> {
     return this.httpService
-      .get('/jobs/byInstance/')
+      .get(this.BACKUP_BASEURL + '/' + instanceId + '/plans?sort=startDate,desc' )
       .map(res => {
         return res.json() as BackupPlan;
       })
       .catch(e => this.httpService.formatError(e));
   }
 
-  public loadRecentBackupJobs(): Observable<{} | Job> {
+  public loadBackupPlan(instanceId: string, planId: string): Observable<{} | BackupPlan> {
     return this.httpService
-      .get('/plans/byInstance/')
+      .get(this.BACKUP_BASEURL + '/' + instanceId + '/plans/' + planId)
+      .map(res => {
+        return res.json() as BackupPlan;
+      })
+      .catch(e => this.httpService.formatError(e));
+  }
+
+  public loadRecentBackupJobs(instanceId: string): Observable<{} | Job> {
+    return this.httpService
+      .get(this.BACKUP_BASEURL + '/' + instanceId + '/jobs?sort=startDate,desc')
       .map(res => {
         return res.json() as Job;
       })
       .catch(e => this.httpService.formatError(e));
   }
 
-  public save(rel, entity: any): Observable<{} | any> {
+  public save(instanceId: string, rel, entity: any): Observable<{} | any> {
     return this.httpService
-      .post('/' + rel, entity)
+      .post(this.BACKUP_BASEURL + '/'  + instanceId + '/'  + rel, entity)
       .map(res => res.json() as any)
       .catch(e => this.httpService.formatError(e));
   }
 
+  public update(instanceId: string, rel: string, entity: any): Observable<{} | any> {
+    return this.httpService.patch(this.BACKUP_BASEURL + '/' + instanceId + '/' + rel + '/' + entity.id, entity)
+      .map(res => res.json() as any)
+      .catch(e => this.httpService.formatError(e));
+  }
+
+  public delete(instanceId: string, rel: string, entity: any): Observable<{} | any> {
+    return this.httpService.delete(this.BACKUP_BASEURL + '/' + instanceId + '/' + rel + '/' + entity.id)
+      .map(res => res.json() as any)
+      .catch(e => this.httpService.formatError(e));
+  }
+
+  public loadBackupJob(instanceId: string, jobId: string) {
+    return this.httpService
+      .get(this.BACKUP_BASEURL + '/' + instanceId + '/jobs/' + jobId )
+      .map(res => {
+        return res.json();
+      })
+      .catch(e => this.httpService.formatError(e));
+  }
 }
